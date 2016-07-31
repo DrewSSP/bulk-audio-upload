b206ce588369612476c3edea70201594369b1f9e
Drew Pilcher <drewpilcher@Drews-MacBook-Pro.local>
2016-07-31 21:21:39 +0900

    pythonized the php script

--- README.md
+++ README.md
@@ -2,15 +2,25 @@
 This script uploads chinese audio for words that don't have any audio uploaded in any course to which you have editor access
 
 ## Set up
-make a file in the root directory of your script called 'variables.py' and create a python dictionary of your cookies. It should look something like this:
+This script has the following requirements:
+* python 3
+* libraries:
+  * requests
+  * tempfile
+  * sys
+  * subprocess
+  * multiprocessing
+  * lxml
+
+You must also make a file in the root directory of your script called 'variables.py' and create a python dictionary of your cookies. It should look something like this:
 ```python
 cookies = {'_sp_id.7bc7': '06d67edb75b999999.1466999953.100.1555544393.1234573782',
            '_sp_ses.7bc7': '*',
-           'csrftoken': 'mxIto89I10jvWe45lt5xBJ8xnQkWayh3',
+           'csrftoken': 'nxIto89I10jvMe45lt5xBJ8xnQkWayh3',
            'fbm_143688012353890': 'base_domain=.www.memrise.com',
-           'fbsr_143688012353890': 'bGNeJtlEkaCEISsa4th4J1-FvygfuhFgBuu3qnnS1M.eyJhbGdvcml03f6OiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUNxNTh6ekpDMDlpNGNpTjNyaVRYR3VEN0xHZHM1TEJyMmFCVlJBTmVHeVl4WFlWanM1QVRpMDBtSzFwMWNFV2dxa28wUUxrWUVoT0RKLVdQOG1DdHVncjZhSGdxMVBEaEp3MGhTbV9pRXA3ckcxZU02d2M0LTNubFFCVkVaU0tUVml3eXFKdy1FZmxJYTVuZGJZUlhBXzVlNXdGcFFKWWVJem83ZllwcV9COWExdkJ0S3ZFQlB6OGQ2c2w0azMyOWtxcmFyVjJRYXpodVh3WXVlbVc4ejNlUUc5TE11SXJsakxTbW1hOXN3cUREVzEtcl94WlNLRlRucklsV3FxY3kzN0g1UnFVeTRwOFZ0TVVSXzEzd0Z0TjBrNF9PZjgzalJHYVZjZkV5dWFqUDJLQVhSRkxmT3RrU0R5d3lRSnRncGRMVC1ZZTVRTkNKN0xNRWxfcEVVWSIsImlzc3VlZF9hdCI6MTQ2ODY0Mzk4MiwidXNlcl9pZCI6IjUwMzc3MTY3OCJ9',
+           'fbsr_143688012353890': 'bGMeJtlEkaCEISsa4th4J1-FvygfuhFgBuu7qnnS1M.eyJhbGdvcml03f6OiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUNxNTh6ekpDMDlpNGNpTjNyaVRYR3VEN0xHZHM1TEJyMmFCVlJBTmVHeVl4WFlWanM1QVRpMDBtSzFwMWNFV2dxa28wUUxrWUVoT0RKLVdQOG1DdHVncjZhSGdxMVBEaEp3MGhTbV9pRXA3ckcxZU02d2M0LTNubFFCVkVaU0tUVml3eXFKdy1FZmxJYTVuZGJZUlhBXzVlNXdGcFFKWWVJem83ZllwcV9COWExdkJ0S3ZFQlB6OGQ2c2w0azMyOWtxcmFyVjJRYXpodVh3WXVlbVc4ejNlUUc5TE11SXJsakxTbW1hOXN3cUREVzEtcl94WlNLRlRucklsV3FxY3kzN0g1UnFVeTRwOFZ0TVVSXzEzd0Z0TjBrNF9PZjgzalJHYVZjZkV5dWFqUDJLQVhSRkxmT3RrU0R5d3lRSnRncGRMVC1ZZTVRTkNKN0xNRWxfcEVVWSIsImlzc3VlZF9hdCI6MTQ2ODY0Mzk4MiwidXNlcl9pZCI6IjUwMzc3MTY3OCJ9',
            'i18next': 'en',
-           'sessionid': 'xrxg3zofonxmfmf5gfdgv5444defa71'}
+           'sessionid': 'xrxg3zofonxnfmf5gfdgv5444defa71'}
 ```
 
 ## To run the script
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import requests, tempfile, sys, subprocess, logging
+import requests, tempfile, sys, subprocess
 from variables import cookies
 from multiprocessing import Pool
-from lxml.html.soupparser import fromstring
 from lxml import html
+
+def upload_file_to_server(thing_id, cell_id, course, file):
+	files = {'f': ('whatever.mp3', open(file.name, 'rb'), 'audio/mp3')}
+	form_data = { 
+		"thing_id": thing_id, 
+		"cell_id": cell_id, 
+		"cell_type": "column",
+		"csrfmiddlewaretoken": cookies['csrftoken']}
+	headers = {
+		"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0",
+		"referer": course}
+	post_url = "http://www.memrise.com/ajax/thing/cell/upload_file/"
+	r = requests.post(post_url, files=files, cookies=cookies, headers=headers, data=form_data, timeout=60)
 
 def get_audio_files_from_course(first_database_page, number_of_pages):
 	database_urls = []
@@ -24,38 +36,32 @@ def get_thing_information(database_url):
 		audio_files = div.xpath("td[contains(@class, 'audio')]/div/div[contains(@class, 'dropdown-menu')]/div")
 		number_of_audio_files = len(audio_files)
 		audios.append({'thing_id': thing_id, 'number_of_audio_files': number_of_audio_files, 'chinese_word': chinese_word, 'column_number_of_audio': column_number_of_audio})
-	upload_audios(audios)
+	sequence_through_audios(audios)
 
 def download_audio(path):
 	response = requests.get(path)
 	if response.status_code == requests.codes.ok:
-		logging.info('Getting ' + path + ': ' + str(response.status_code))
 		temp_file = tempfile.NamedTemporaryFile(suffix='.mp3')
 		temp_file.write(response.content)
 		return temp_file
 	else:
 		return False
 
-def upload_audios(audios):
+def sequence_through_audios(audios):
 	for audio in audios:
 		if audio['number_of_audio_files'] > 0:
-			logging.info('SKIPPED: ' + audio['chinese_word'] + ' - Audio files already exist')
 			continue
 		else:
 			print('Adding audio to ' + audio['chinese_word'])
 			requests.post('http://soundoftext.com/sounds', data={'text':audio['chinese_word'], 'lang':'zh-CN'}) # warn the server of what file I'm going to need
 			temp_file = download_audio('http://soundoftext.com/static/sounds/zh-CN/' + audio['chinese_word'] + '.mp3') #download audio file
 			if temp_file == False:
-				logging.warning('SKIPPED -- soundoftext.com returned a bad response code on ' + audio['chinese_word'])
 				continue
 			else:
-				subprocess.call(['php', 'upload.php', audio['thing_id'], temp_file.name, course_database_url, audio['column_number_of_audio']]) # upload audio
+				upload_file_to_server(audio['thing_id'], audio['column_number_of_audio'], course_database_url, temp_file)
 				temp_file.close()
 
 if __name__ == "__main__":
-	logging.basicConfig(filename='main.log',level=logging.DEBUG)
-	# logging.getLogger("urllib3").setLevel(logging.CRITICAL)
-	# logging.getLogger("requests").setLevel(logging.WARNING)
 	course_database_url = sys.argv[1]
 	number_of_pages = int(html.fromstring(requests.get(course_database_url, cookies=cookies).content).xpath("//div[contains(@class, 'pagination')]/ul/li")[-2].text_content())
 	get_audio_files_from_course(course_database_url, number_of_pages)
